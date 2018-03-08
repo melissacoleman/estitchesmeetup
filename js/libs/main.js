5fc804fdc89c6c2368699e444bb89216c1834eba
Melissa Coleman <info@melissacoleman.nl>
2018-03-08 12:59:13 +0000

    update styles of map

--- js/libs/main.js
+++ js/libs/main.js
@@ -14,68 +14,281 @@ function initialize() {
 
         styles: [
             {
-                featureType: "landscape",
+                elementType: "geometry",
                 stylers: [
-                    { saturation: -100 },
-                    { lightness: 65 },
-                    { visibility: "on" }
+                    {
+                        color: "#f5f5f5"
+                    }
+                ]
+            },
+            {
+                elementType: "labels.icon",
+                stylers: [
+                    {
+                        visibility: "off"
+                    }
+                ]
+            },
+            {
+                elementType: "labels.text.fill",
+                stylers: [
+                    {
+                        color: "#616161"
+                    }
+                ]
+            },
+            {
+                elementType: "labels.text.stroke",
+                stylers: [
+                    {
+                        color: "#f5f5f5"
+                    }
+                ]
+            },
+            {
+                featureType: "administrative.land_parcel",
+                elementType: "labels",
+                stylers: [
+                    {
+                        visibility: "off"
+                    }
+                ]
+            },
+            {
+                featureType: "administrative.land_parcel",
+                elementType: "labels.text.fill",
+                stylers: [
+                    {
+                        color: "#bdbdbd"
+                    }
                 ]
             },
             {
                 featureType: "poi",
+                elementType: "geometry",
                 stylers: [
-                    { saturation: -100 },
-                    { lightness: 51 },
-                    { visibility: "simplified" }
+                    {
+                        color: "#eeeeee"
+                    }
+                ]
+            },
+            {
+                featureType: "poi",
+                elementType: "labels.text",
+                stylers: [
+                    {
+                        visibility: "off"
+                    }
+                ]
+            },
+            {
+                featureType: "poi",
+                elementType: "labels.text.fill",
+                stylers: [
+                    {
+                        color: "#757575"
+                    }
+                ]
+            },
+            {
+                featureType: "poi.business",
+                stylers: [
+                    {
+                        visibility: "off"
+                    }
+                ]
+            },
+            {
+                featureType: "poi.park",
+                elementType: "geometry",
+                stylers: [
+                    {
+                        color: "#e5e5e5"
+                    }
+                ]
+            },
+            {
+                featureType: "poi.park",
+                elementType: "labels.text.fill",
+                stylers: [
+                    {
+                        color: "#9e9e9e"
+                    }
+                ]
+            },
+            {
+                featureType: "road",
+                elementType: "geometry",
+                stylers: [
+                    {
+                        color: "#ffffff"
+                    }
+                ]
+            },
+            {
+                featureType: "road",
+                elementType: "labels.icon",
+                stylers: [
+                    {
+                        visibility: "off"
+                    }
+                ]
+            },
+            {
+                featureType: "road.arterial",
+                elementType: "labels.text.fill",
+                stylers: [
+                    {
+                        color: "#757575"
+                    }
                 ]
             },
             {
                 featureType: "road.highway",
-                stylers: [{ saturation: -100 }, { visibility: "simplified" }]
+                elementType: "geometry",
+                stylers: [
+                    {
+                        color: "#dadada"
+                    }
+                ]
             },
             {
-                featureType: "road.arterial",
+                featureType: "road.highway",
+                elementType: "labels.text.fill",
                 stylers: [
-                    { saturation: -100 },
-                    { lightness: 30 },
-                    { visibility: "on" }
+                    {
+                        color: "#616161"
+                    }
                 ]
             },
             {
                 featureType: "road.local",
+                elementType: "labels",
                 stylers: [
-                    { saturation: -100 },
-                    { lightness: 40 },
-                    { visibility: "on" }
+                    {
+                        visibility: "off"
+                    }
+                ]
+            },
+            {
+                featureType: "road.local",
+                elementType: "labels.text.fill",
+                stylers: [
+                    {
+                        color: "#9e9e9e"
+                    }
                 ]
             },
             {
                 featureType: "transit",
-                stylers: [{ saturation: -100 }, { visibility: "simplified" }]
-            },
-            {
-                featureType: "administrative.province",
-                stylers: [{ visibility: "off" }]
-            },
-            {
-                featureType: "water",
-                elementType: "labels",
                 stylers: [
-                    { visibility: "on" },
-                    { lightness: -25 },
-                    { saturation: -100 }
+                    {
+                        visibility: "off"
+                    }
+                ]
+            },
+            {
+                featureType: "transit.line",
+                elementType: "geometry",
+                stylers: [
+                    {
+                        color: "#e5e5e5"
+                    }
+                ]
+            },
+            {
+                featureType: "transit.station",
+                elementType: "geometry",
+                stylers: [
+                    {
+                        color: "#eeeeee"
+                    }
                 ]
             },
             {
                 featureType: "water",
                 elementType: "geometry",
                 stylers: [
-                    { hue: "#ffff00" },
-                    { lightness: -25 },
-                    { saturation: -97 }
+                    {
+                        color: "#c9c9c9"
+                    }
+                ]
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.fill",
+                stylers: [
+                    {
+                        color: "#9e9e9e"
+                    }
                 ]
             }
         ]
+
+        // styles: [
+        //     {
+        //         featureType: "landscape",
+        //         stylers: [
+        //             { saturation: -100 },
+        //             { lightness: 65 },
+        //             { visibility: "on" }
+        //         ]
+        //     },
+        //     {
+        //         featureType: "poi",
+        //         stylers: [
+        //             { saturation: -100 },
+        //             { lightness: 51 },
+        //             { visibility: "simplified" }
+        //         ]
+        //     },
+        //     {
+        //         featureType: "road.highway",
+        //         stylers: [{ saturation: -100 }, { visibility: "simplified" }]
+        //     },
+        //     {
+        //         featureType: "road.arterial",
+        //         stylers: [
+        //             { saturation: -100 },
+        //             { lightness: 30 },
+        //             { visibility: "on" }
+        //         ]
+        //     },
+        //     {
+        //         featureType: "road.local",
+        //         stylers: [
+        //             { saturation: -100 },
+        //             { lightness: 40 },
+        //             { visibility: "on" }
+        //         ]
+        //     },
+        //     {
+        //         featureType: "transit",
+        //         stylers: [{ saturation: -100 }, { visibility: "simplified" }]
+        //     },
+        //     {
+        //         featureType: "administrative.province",
+        //         stylers: [{ visibility: "off" }]
+        //     },
+        //     {
+        //         featureType: "water",
+        //         elementType: "labels",
+        //         stylers: [
+        //             { visibility: "on" },
+        //             { lightness: -25 },
+        //             { saturation: -100 }
+        //         ]
+        //     },
+        //     {
+        //         featureType: "water",
+        //         elementType: "geometry",
+        //         stylers: [
+        //             { hue: "#ffff00" },
+        //             { lightness: -25 },
+        //             { saturation: -97 }
+        //         ]
+        //     }
+        // ]
     };
 
     var map = new google.maps.Map(
